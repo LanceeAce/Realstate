c8a20cb34f8def33ecef71694c16211f8c33c79c
Erano Lorido <erdielorido02@yahoo.com>
2025-03-12 16:42:59 +0800

    responsive

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,40 +3,42 @@ import imgBuilding from "./assets/building.png";
 const About = () => {
   return (
     <div id="about">
-    <div>
-      <div className="flex justify-between items-center px-[10rem] pt-[3rem] pb-20 ">
-        <div className="w-[80rem] pr-[5rem]">
-          <h2 className="text-[3rem] font-semibold text-Poppins pb-[2rem]">
-            We are <span className="font-bold">TechState</span>
-          </h2>
-          <br />
+      <div>
+        <div className="flex flex-col lg:flex-row justify-center items-center px-[2rem] lg:px-[10rem] pt-[3rem] pb-20">
+          <div className="w-full lg:w-[80rem] pr-[5rem]">
+            <h2 className="text-[2.5rem] lg:text-[3rem] font-semibold text-Poppins pb-[2rem] text-center lg:text-left">
+              We are <span className="font-bold">TechState</span>
+            </h2>
 
-          <p className="text-[grey]  ">
-            Welcome to Property Street, your gateway to a world of exceptional
-            real estate opportunities. At Property Street, we understand that
-            finding the perfect home or investment property is more than a
-            transaction—it's a significant milestone in your life. <br />
-            <br />
-            Our platform is designed to simplify your property search, providing
-            a seamless experience as you explore a diverse range of apartments,
-            villas, plots, and more.
-          </p>
-          <br />
+            <p className="text-[grey] text-center lg:text-left">
+              Welcome to Property Street, your gateway to a world of exceptional
+              real estate opportunities. At Property Street, we understand that
+              finding the perfect home or investment property is more than a
+              transaction—it's a significant milestone in your life. <br />
+              <br />
+              Our platform is designed to simplify your property search,
+              providing a seamless experience as you explore a diverse range of
+              apartments, villas, plots, and more.
+            </p>
+            <div className="text-center lg:text-left mt-6">
+              <a
+                href="#"
+                className="bg-[#333333] hover:bg-white hover:border-2 hover:border-gray-800 hover:text-black rounded-[.6rem] px-[2rem] py-[0.5rem] text-white"
+              >
+                Read more
+              </a>
+            </div>
+          </div>
 
-          <div className="">
-            <a
-              href="#"
-              className="bg-[#333333] hover:bg-white hover:border-2 hover:border-gray-800 hover:text-black rounded-[.6rem] px-[2rem] py-[0.5rem] text-white "
-            >
-              Read more
-            </a>
+          <div className="mt-6 lg:mt-0">
+            <img
+              src={imgBuilding}
+              alt="Building pic"
+              className="w-full max-w-[1000px] h-auto"
+            />
           </div>
         </div>
-        <div className="">
-          <img src={imgBuilding} alt="Building pic" width={1000} height={350} />
-        </div>
       </div>
-    </div>
     </div>
   );
 };
--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -5,60 +5,57 @@ import icon3 from "./assets/icon3.png";
 
 const Description = () => {
   return (
-    <div id="service">
-    <div className="block md:flex justify-between  pb-[10rem]  gap-20">
-      <div className="w-[50rem] flex ">
-        <img src={imgHouse} alt="House picture" width={700} height={200} />
+    <div className="flex flex-col lg:flex-row justify-between pt-[11.5rem] pb-[1rem] px-[5rem] lg:px-[15rem] gap-20">
+      <div className="w-full lg:w-[50rem] flex justify-center">
+        <img
+          src={imgHouse}
+          alt="House picture"
+          className="w-full max-w-[700px] h-auto"
+        />
       </div>
 
-      <div className="block w-full">
+      <div className="w-full">
         <div className="pb-[2rem]">
-          <h2 className="text-[2.5rem] font-semibold">
+          <h2 className="text-[2.5rem] font-semibold text-center lg:text-left">
             We've got new properties for everyone
           </h2>
-          <br />
-          <p className="text-[1rem]">One Place for everyone needs.</p>
+          <p className="text-[1rem] text-center lg:text-left">
+            One Place for everyone needs.
+          </p>
         </div>
 
         <div>
-          <div className="flex items-center">
-            <div>
-              <img src={icon1} alt="Icon 1" className="pr-[1rem]" />
+          <div className="flex flex-col lg:flex-row items-center mb-6">
+            <div className="mb-4 lg:mb-0 lg:pr-[1rem]">
+              <img src={icon1} alt="Icon 1" className="w-[40px] h-[40px]" />
             </div>
-
-            <br />
-
-            <div className="">
-              <p>600+ Apartments</p>
+            <div>
+              <p className="font-semibold">600+ Apartments</p>
               <p>Explore stylish apartments for modern living.</p>
             </div>
           </div>
-          <br />
 
-          <div className="flex">
-            <div className="pr-[1rem]">
-              <img src={icon2} alt="Icon 2" />
+          <div className="flex flex-col lg:flex-row items-center mb-6">
+            <div className="mb-4 lg:mb-0 lg:pr-[1rem]">
+              <img src={icon2} alt="Icon 2" className="w-[40px] h-[40px]" />
             </div>
             <div>
-              <p>250+ Plots</p>
+              <p className="font-semibold">250+ Plots</p>
               <p>Find the perfect space with us.</p>
             </div>
           </div>
-          <br />
 
-          <div className="flex">
-            <div>
-              <img src={icon3} alt="Icon 3" className="pr-[1rem]" />
+          <div className="flex flex-col lg:flex-row items-center mb-6">
+            <div className="mb-4 lg:mb-0 lg:pr-[1rem]">
+              <img src={icon3} alt="Icon 3" className="w-[40px] h-[40px]" />
             </div>
             <div>
-              <p>60+ Villas</p>
+              <p className="font-semibold">60+ Villas</p>
               <p>Your dream villa awaits—explore with us.</p>
             </div>
           </div>
 
-          <br />
-
-          <div className="">
+          <div className="text-center lg:text-left">
             <a
               href="#"
               className="bg-[#333333] text-white rounded-[1rem] px-[2.5rem] py-[0.5rem] hover:bg-white hover:border-gray-800 hover:border-2 hover:text-black"
@@ -68,7 +65,6 @@ const Description = () => {
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
